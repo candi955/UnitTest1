4a157d5badd3fcc571536a71db13d7b1e80df645
candi955 <44098788+candi955@users.noreply.github.com>
2020-01-10 20:05:06 -0700

    Completed the class NumStack() and function _intStack_ to show an empty data structure, and then #show the concept of push utilizing append().

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 # https://stackoverflow.com/questions/46801747/peek-stack-in-python-3
 # for stacks and queues, push and pop:
 # https://www.geeksforgeeks.org/using-list-stack-queues-python/
+# https://dbader.org/blog/stacks-in-python
 # for sets and frozensets: https://www.python-course.eu/sets_frozensets.php
 # for modulo: https://www.java67.com/2014/11/modulo-or-remainder-operator-in-java.html
 # https://medium.com/programminginpython-com/python-program-to-find-whether-an-integer-is-even-or-odd-number-a028da555922
@@ -25,6 +26,19 @@
 # the equation I utilized.  I also placed the reference in the formerly mentioned project references above these notes.
 # I have begun to create the actual stack under class NumStack(), which I plan to create an array to demonstrate the
 # concepts of push, pop, and peek on the First In Last Out (FILO) stack in array format.
+# 1/10/2020 @ 19:30pm - I checked on the assignment to ensure I was following the instructions.  It stated to ensure that
+# the following was tested and handled properly:
+# 1) Pop and Peek from an empty stack (no values are stored on the stack)
+# 2) Push to a stack that is full (no space is left in the array)
+# In order to accomplish this, I began following the reference https://dbader.org/blog/stacks-in-python to create a new
+# class NumStack() and function _intStack_ to show an empty data structure, and then show the concept of push utilizing append().
+# It appears I might perform some of the steps out of order, but I am attempting to complete all steps per my best
+# understanding of their functions as I learn.
+# 1/10/2020 @ 19:54pm - I have completed the class NumStack() and function _intStack_ to show an empty data structure, and then
+# show the concept of push utilizing append(). Next I plan on displaying how to pop the numbers from the data structure and then
+# the failed function of popping from an empty data structure.  I am first going to attempt displaying this concept, and plan to
+# attempt peek later after learning how to pop the numbers in the stack.
+# I also am making another commit on Git to my Github repository.
 
 class TestStack():
     def unitTestStackEven(self):
@@ -33,9 +47,9 @@ class TestStack():
         # reference for the idea of creating the below variable and equation that is intended to result in an even number:
         # https://medium.com/programminginpython-com/python-program-to-find-whether-an-integer-is-even-or-odd-number-a028da555922
         if testNum(4) % 2 == 0:
-            return print("Test is successful!")
+            return print("Test is successful!\n")
         else:
-            return print("Test is not successful")
+            return print("Test is not successful\n")
 
     def unitTestStackFail(self):
         testNum = int
@@ -43,21 +57,86 @@ class TestStack():
         # reference for the idea of creating the below variable and equation that is intended to result in an even number:
         # https://medium.com/programminginpython-com/python-program-to-find-whether-an-integer-is-even-or-odd-number-a028da555922
         if testNum(3) % 2 == 0:
-            return print("Test is successful!")
+            return print("Test is successful!\n")
         else:
-            return print("Test is not successful")
+            return print("Test is not successful\n")
 
 TestStack()
 
 class NumStack():
+    def _intStack_(self):
+        # reference for append to stack: https://dbader.org/blog/stacks-in-python
+
+        intStackLIFO = []
+
+        intStackLIFO.append(2)
+        intStackLIFO.append(4)
+
+        print("Below I have added two numbers utilizing push(append) to the iStackLIFO stack:\n")
+        print(intStackLIFO)
+        print("\n")
 
 
+
+        intStackLIFO.append(6)
+        intStackLIFO.append(7)
+
+        print("Below I have added two more numbers (four numbers total) utilizing push(append) to the iStackLIFO stack:\n")
+        print(intStackLIFO)
+        print("\n")
+        # reference for append to stack: https://dbader.org/blog/stacks-in-python
+
+        class NumStack():
+            def _intStack_(self):
+                # reference for append to stack: https://dbader.org/blog/stacks-in-python
+
+                intStackLIFO = []
+
+                intStackLIFO.append(2)
+                intStackLIFO.append(4)
+
+                print("Below I have added two numbers utilizing push(append) to the iStackLIFO stack:\n")
+                print(intStackLIFO)
+                print("\n")
+
+                intStackLIFO.append(6)
+                intStackLIFO.append(7)
+
+                print(
+                    "Below I have added two more numbers (four numbers total) utilizing push(append) to the iStackLIFO stack:\n")
+                print(intStackLIFO)
+                print("\n")
+                # reference for append to stack: https://dbader.org/blog/stacks-in-python
+
+            def _intStack_(self):
+                # reference for append to stack: https://dbader.org/blog/stacks-in-python
+
+                intStackLIFO = []
+
+                intStackLIFO.append(2)
+                intStackLIFO.append(4)
+
+                print("Below I have added two numbers utilizing push(append) to the iStackLIFO stack:\n")
+                print(intStackLIFO)
+                print("\n")
+
+                intStackLIFO.append(6)
+                intStackLIFO.append(7)
+
+                print(
+                    "Below I have added two more numbers (four numbers total) utilizing push(append) to the iStackLIFO stack:\n")
+                print(intStackLIFO)
+                print("\n")
+                # reference for append to stack: https://dbader.org/blog/stacks-in-python
+
+NumStack()
 
 
 
 def main():
 
     test = TestStack()
+    intStackLIFO_orig = NumStack()
 
     print("\nTest results for the even number of 4 (should be successful if program functioning properly):")
     test.unitTestStackEven()
@@ -66,6 +145,8 @@ def main():
     print("\nTest results for the even number of 3 (should fail if program functioning properly):")
     test.unitTestStackFail()
     print("\n")
+
+    intStackLIFO_orig._intStack_()
 
 main()
 
